14081cdfc334c65771f58ef41d4db19bbd78d088
jelmervandijk <99044376@mydavinci.nl>
2019-02-19 09:39:40 +0100

    kan door de vragen heen gaan

--- js/script.js
+++ js/script.js
@@ -1,27 +1,50 @@
-var eens = document.getElementById('eens');
-var test = document.getElementById('testtekst')
-var lock = document.getElementById('lock');
-var eye = document.getElementById('eye');
-var imgLogo = document.getElementById('imglogo');
-var textlogo = document.getElementById('texttitel');
-var neutraal = document.getElementById('neutraal');
-var oneens = document.getElementById('oneens');
-var container = document.getElementById('container');
-var titel = document.getElementById('')
-var h2 = document.getElementById('');
 var start = document.getElementById('start');
+var answerbox = document.getElementById('answerbox');
 var counter = 0;
 var answers = [];
+
+
 start.addEventListener("click", function(){
-	imglogo.style.visibility ="hidden";
-	test.style.visibility ="hidden";
-	textlogo.style.visibility ="hidden";
-	eye.style.visibility ="hidden";
-	start.style.display ="none";
-	start.style.visibility ="hidden";
-	lock.style.visibility ="hidden";
-	eens.style.visibility ="visible";
-	oneens.style.visibility="visible";
-	neutraal.style.visibility="visible";	
+	start.classList.add("hidden"); 
+	backImg.classList.add("show");
+	answerbox.style.visibility = "visible";
+	title.innerHTML = subjects[0].title;
+	statement.innerHTML = subjects[0].statement;
 })
 
+
+pro.addEventListener("click", function() {
+	endScreen();
+	answers.push("pro");
+	questions();
+})
+
+contra.addEventListener("click", function() {
+	endScreen();
+	answers.push("contra");
+	questions();
+})
+ambivalent.addEventListener("click", function() {
+	endScreen();
+	answers.push("ambivalent");
+	questions();
+})
+
+skip.addEventListener("click", function() {
+	endScreen();
+	answers.push("neither");
+	questions();
+})
+
+function endScreen() {
+    if (counter == 7) {
+           answerbox.classList.add("hidden");
+           title.classList.add("hidden");
+           statement.classList.add("hidden");
+       }
+}
+function questions() {
+      counter++;
+        document.getElementById('title').innerHTML = subjects[counter].title;
+        document.getElementById('statement').innerHTML = subjects[counter].statement;
+}
