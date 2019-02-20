e21ab17427416223c712b21498f5c8f32c307c5b
jelmervandijk <99044376@mydavinci.nl>
2019-02-20 14:10:12 +0100

    de beste catogorie laten zien

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ var answerbox = document.getElementById('answerbox');
 var counter = 0;
 var answers = [];
 
-
 start.addEventListener("click", function(){
 	start.classList.add("hidden"); 
 	backImg.classList.add("show");
@@ -40,11 +39,43 @@ function endScreen() {
     if (counter == 7) {
            answerbox.classList.add("hidden");
            title.classList.add("hidden");
-           statement.classList.add("hidden");
+           statement.classList.add("hidden");	
+           pro.classList.add("hidden");
+           ambivalent.classList.add("hidden");
+           contra.classList.add("hidden");
+           skip.classList.add("hidden");
+           titel.classList.add("show");
+           var text = "";
+           for (i = 0; i < subjects.length; i++) {
+           	text +=  subjects[i].title + "<br>";
+           }
+          document.getElementById("important").innerHTML = text;
+
        }
+
 }
+
 function questions() {
       counter++;
-        document.getElementById('title').innerHTML = subjects[counter].title;
-        document.getElementById('statement').innerHTML = subjects[counter].statement;
+        title.innerHTML = subjects[counter].title;
+        statement.innerHTML = subjects[counter].statement;
 }
+
+function goBack() {
+	counter--
+	title.innerHTML = subjects[counter].title;
+	statement.innerHTML = subjects[counter].statement;
+}
+
+function goHome() {
+	if (counter == 0) {
+		start.classList.add("show"); 
+		backImg.classList.add("hidden");
+		answerbox.style.visibility = "hidden";
+
+	}
+}
+
+
+
+
